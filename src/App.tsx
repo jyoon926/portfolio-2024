e5982ee67556a5e0c0762f0a46f773d04ddd558e
jyoon926 <jacobyoon926@gmail.com>
2024-10-16 00:12:31 -0400

    Remove redundant import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.scss';
 import Menu from './components/Menu';
 import { ColorSchemes, Sections } from './Data';
