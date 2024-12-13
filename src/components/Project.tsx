4821ff7423a9ec7a5fdf2569a23943232d2dc65e
jyoon926 <jacobyoon926@gmail.com>
2024-12-13 12:01:15 -0500

    Make main project photo a button

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -46,9 +46,9 @@ export default function Project() {
             <p className="opacity-50 text-center">Project Overview</p>
             <p className="text-4xl text-center">{project.title}</p>
             <p className="text-center">{project.date}</p>
-            <div className="my-10 lg:w-3/4 border bg-foreground/15 p-5 lg:p-10" key={index}>
+            <button className="my-10 lg:w-3/4 border bg-foreground/15 p-5 lg:p-10 duration-300 hover:bg-foreground/25" onClick={() => handlePhotoClick(0)}>
               <img className="w-full" src={project.images[0]} alt="" />
-            </div>
+            </button>
             <div className="w-full mb-8 flex flex-col md:flex-row justify-center gap-3">
               <p className="md:w-1/4 max-w-[200px] opacity-50">About</p>
               <p className="md:w-3/4 max-w-[600px] leading-normal">{project.description}</p>
