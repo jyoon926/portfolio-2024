3afd17d02d36009d9726f7fbc4a7d96f58795573
jyoon926 <jacobyoon926@gmail.com>
2024-12-12 20:22:52 -0500

    Improve intro section

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -38,46 +38,35 @@ export default function Intro({ onTabClick }: Props) {
 
   return (
     <div className="relative">
-      <div className="min-h-screen w-full flex flex-col md:flex-row items-start justify-start gap-10 p-3 pt-20">
-        <div className="w-full md:w-1/3 flex flex-col gap-10">
-          <div className="flex flex-col gap-3">
-            <div className="relative">
-              {/* Gradients */}
-              <div className={`gradient-left ${!showLeftGradient && 'opacity-0'}`}></div>
-              <div className={`gradient-right ${!showRightGradient && 'opacity-0'}`}></div>
-              {/* Tabs */}
-              <div
-                ref={tabContainerRef}
-                className="flex flex-row gap-1 text-nowrap overflow-x-auto overflow-y-hidden scrollbar-hidden"
-              >
-                {Intros.map((intro, index) => (
-                  <div key={index}>
-                    <button
-                      className={`pr-1 whitespace-nowrap transition-opacity hover:opacity-100 ${currentTab !== index && 'opacity-50'}`}
-                      onClick={() => setCurrentTab(index)}
-                    >
-                      {intro.label}
-                    </button>
-                    {index < Intros.length - 1 && <span className="opacity-50">/</span>}
-                  </div>
-                ))}
-              </div>
+      <div className="min-h-screen w-full flex flex-col items-center p-3 py-32">
+        <div className="w-full max-w-[800px] flex flex-col items-start justify-start gap-5 overflow-hidden">
+          <div className="w-full relative">
+            {/* Gradients */}
+            <div className={`gradient-left ${!showLeftGradient && 'opacity-0'}`}></div>
+            <div className={`gradient-right ${!showRightGradient && 'opacity-0'}`}></div>
+            {/* Tabs */}
+            <div
+              ref={tabContainerRef}
+              className="flex flex-row gap-1 text-nowrap overflow-x-auto overflow-y-hidden scrollbar-hidden"
+            >
+              {Intros.map((intro, index) => (
+                <div key={index}>
+                  <button
+                    className={`pr-1 whitespace-nowrap transition-opacity hover:opacity-100 ${currentTab !== index && 'opacity-50'}`}
+                    onClick={() => setCurrentTab(index)}
+                  >
+                    {intro.label}
+                  </button>
+                  {index < Intros.length - 1 && <span className="opacity-50">/</span>}
+                </div>
+              ))}
             </div>
-            <div className="leading-snug">{Intros[currentTab].text}</div>
           </div>
-          <img className="w-56 mix-blend-luminosity rounded-md border" src="/headshot.jpg" alt="" />
-        </div>
-        <div className="w-full md:w-2/3 flex flex-col items-start gap-3 pb-20">
-          <p className="max-w-[800px] leading-snug">
-            I am a fourth-year computer science student at RIT pursuing a combined BS/MS degree. I've cultivated a
-            strong foundation in both theoretical knowledge and practical application. My academic journey has been
-            complemented by two six-month co-ops as a software engineer intern at Dassault Systèmes, where I gained
-            extensive hands-on industry experience in full stack development. My love for art and design enhances my
-            ability to create digital experiences that are not only functionally robust but also visually compelling.
-            Whether it's building responsive web applications or solving complex technical challenges, I approach every
-            project with a growth mindset, continuously seeking to improve and innovate.
+          <div className="leading-[1.1] text-4xl pb-5">{Intros[currentTab].text}</div>
+          <p className="leading-snug">
+            As a fourth-year BS/MS student in computer science at RIT, I blend technical expertise with creative vision. My academic foundation is reinforced by real-world experience from two six-month software engineering co-ops at Dassault Systèmes, where I honed my full stack development skills. Drawing from my passion for art and design, I craft digital experiences that seamlessly unite functionality with aesthetics. I thrive on technical challenges and approach software development as a continuous journey of innovation, always pushing the boundaries of what's possible through code.
           </p>
-          <div className="flex flex-row gap-5">
+          <div className="flex flex-row gap-5 pb-5">
             <button className="button" onClick={() => onTabClick(1)}>
               View Projects <IoArrowDownSharp />
             </button>
@@ -85,9 +74,10 @@ export default function Intro({ onTabClick }: Props) {
               Resume <IoArrowDownSharp className="rotate-[-135deg]" />
             </a>
           </div>
+          <img className="w-56 mix-blend-luminosity rounded-md border" src="/headshot.jpg" alt="" />
         </div>
       </div>
       <Star />
-    </div>
+    </div >
   );
 }
